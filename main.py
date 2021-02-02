5b82cbf102e37f8365d8680a29682b47dc8c8d0c
Igor Kartashov <you@example.com>
2021-02-02 18:21:32 +0300

    Added tg bot

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello')
+import os
+import time
+
+from telegram import Bot
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+
+def main():
+    bot = Bot(token=BOT_TOKEN)
+
+    while True:
+        try:
+            bot.send_message(chat_id=CHAT_ID, text = "ping from heroku")
+
+        except:
+            print('Error')
+
+        time.sleep(20*60)
+if __name__ =='__main__':
+    main()
